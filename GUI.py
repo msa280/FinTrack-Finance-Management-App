4a77832c6dd934c9fc8701759bb99ed720bcd75a
msa280 <msa280@uclive.ac.nz>
2023-08-10 18:03:43 +1200

    More Code
    Setup Done

--- GUI.py
+++ GUI.py
@@ -6,6 +6,15 @@ def window():
     app = QApplication(sys.argv)
     win = QMainWindow()
     win.setGeometry(200, 200, 300, 300)
+    win.setWindowTitle("FinTrack")
+    
+    label = QtWidgets.QLabel(win)
+    label.setText("My first Label!")
+    label.move(0, 0)
+    
+    win.show()
+    sys.exit(app.exec_())
+     
 window()
     
     
