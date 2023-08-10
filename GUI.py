a3219f757ac9f31558c59fc7ba1fe3c862ba217c
msa280 <msa280@uclive.ac.nz>
2023-08-10 17:59:03 +1200

    Creating GUI
    Added code for initiating the window.

---
+++ GUI.py
@@ -0,0 +1,11 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+
+def window():
+    app = QApplication(sys.argv)
+    win = QMainWindow()
+    win.setGeometry(200, 200, 300, 300)
+window()
+    
+    
