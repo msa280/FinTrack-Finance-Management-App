47928390fd7ce26fa33a5e980c92f025a82e37cb
msa280 <msa280@uclive.ac.nz>
2023-08-10 19:39:56 +1200

    Added Classes

--- GUI.py
+++ GUI.py
@@ -2,15 +2,33 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super(MyWindow, self).__init__()
+        self.setGeometry(200, 200, 300, 300)
+        self.setWindowTitle("FinTrack")        
+        self.initUI()
+        
+    def initUI(self):
+        self.label = QtWidgets.QLabel(self)
+        self.label.setText("My first Label!")
+        self.label.move(50, 50)
+        
+        self.b1 = QtWidgets.QPushButton(self)
+        self.b1.setText("Click Me")
+        self.b1.clicked.connect(self.clicked)
+        
+    def clicked(self):
+        self.label.setText("You Pressed the Button")
+        self.update()
+        
+    def update(self):
+        self.label.adjustSize()
+        
+
 def window():
     app = QApplication(sys.argv)
-    win = QMainWindow()
-    win.setGeometry(200, 200, 300, 300)
-    win.setWindowTitle("FinTrack")
-    
-    label = QtWidgets.QLabel(win)
-    label.setText("My first Label!")
-    label.move(0, 0)
+    win = MyWindow()
     
     win.show()
     sys.exit(app.exec_())
