fdcdf717cb9dbed13970b3f6b770595e2cbd3d80
Haider Saeed <msa280@uclive.ac.nz>
2023-08-11 03:45:03 +1200

    Creating UI using PyQt5 Designer
    Added Interface

--- GUI.py
+++ GUI.py
@@ -2,37 +2,37 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
+
 class MyWindow(QMainWindow):
     def __init__(self):
         super(MyWindow, self).__init__()
         self.setGeometry(200, 200, 300, 300)
-        self.setWindowTitle("FinTrack")        
+        self.setWindowTitle("FinTrack")
         self.initUI()
-        
+
     def initUI(self):
         self.label = QtWidgets.QLabel(self)
         self.label.setText("My first Label!")
         self.label.move(50, 50)
-        
+
         self.b1 = QtWidgets.QPushButton(self)
         self.b1.setText("Click Me")
         self.b1.clicked.connect(self.clicked)
-        
+
     def clicked(self):
         self.label.setText("You Pressed the Button")
         self.update()
-        
+
     def update(self):
         self.label.adjustSize()
-        
+
 
 def window():
     app = QApplication(sys.argv)
     win = MyWindow()
-    
+
     win.show()
     sys.exit(app.exec_())
-     
+
+
 window()
-    
-    
